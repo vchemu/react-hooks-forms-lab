2736c54b46a320cecedf97a613876af716b826b2
valerine <valerine.chemutai@student.moringaschool.com>
2023-09-04 22:47:59 +0300

    controlled

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,28 @@ import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
 
+
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  
+  function handleItemFormSubmit(newItem) {
+    setItems([
+      ...items,
+      newItem
+    ]);
+  }
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleItemFormSubmit} />
     </div>
   );
 }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ search, onCategoryChange, onSearchChange }) {
+
+  function handleSearchChange(event) {
+    onSearchChange(event.target.value);
+  }
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input type="text" 
+      name="search" 
+      placeholder="Search..."
+      value={search}
+      onChange={handleSearchChange}
+       />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
